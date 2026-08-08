b5d7d6895558c3c75f0f68dc38f0601ede11a018
novalCandra <dinatanovel5@gmail.com>
2026-08-08 17:35:19 +0700

    feat : ⚒️ local state zoom out (state)

--- src/app/board/[id]/page.tsx
+++ src/app/board/[id]/page.tsx
@@ -1,17 +1,33 @@
+'use client'
 import Navbar from '../../../components/ui/Navbar';
-import Toolbar from '../../../components/ui/Toolbar';
-import WhiteboardCanvas from '../../../components/canvas/WhiteboardCanvas';
-import PropertyPanel from '../../../components/properties/PropertyPanel';
+import WhiteboardCanvas from '@/src/components/canvas/WhiteboardCanvas';
+import ButtonZoom from '@/src/components/ui/button/ButtonZoom';
+import SudidebrColors from '@/src/components/ui/Sidebar';
+import { useState } from 'react';
 
 export default function BoardPage() {
+  const [zoom, setZoom] = useState<number>(100);
+  const minZoom: number = 75;
+  const maxZoom: number = 250;
+  const step: number = 10;
+
+  const handleZoom = () => {
+    setZoom(prev => Math.min(prev + step, maxZoom))
+  }
+
+  const handleOutZoom = () => {
+    setZoom(prev => Math.min(prev - step, minZoom))
+  }
   return (
     <div className="flex h-screen flex-col overflow-hidden bg-gray-50">
       <Navbar />
-      <div className="relative flex-1">
+      <div className="relative flex-1" style={{
+        transform: `scale(${zoom / 100})`,
+      }}>
         <WhiteboardCanvas />
-        <Toolbar />
-        <PropertyPanel />
+        <SudidebrColors />
       </div>
+      <ButtonZoom zoom={zoom} handleOutZoom={handleOutZoom} handleZoom={handleZoom} />
     </div>
   );
 }
