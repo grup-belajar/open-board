5b988656dc49e618e576076d89eea9d3c267e312
novalCandra <dinatanovel5@gmail.com>
2026-09-10 21:21:33 +0700

    feat : ✅ Add config (ESLINT)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,11 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  ...compat.extends("next/core-web-vitals", "next/typescript", {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  }),
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
