c7ab717468f21b24d4a77304a34f4ce9a254b0a9
novalCandra <dinatanovel5@gmail.com>
2026-09-10 21:20:58 +0700

    feat : ✅ Add Store History (History)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import canvasReducer from './slices/canvasSlice';
 import toolReducer from './slices/toolSlice';
+import historyReducer from "./slices/historySlice"
 import { indexedDbSyncMiddleware } from './middleware/indexedDbSync';
 
 export const store = configureStore({
   reducer: {
     canvas: canvasReducer,
     tool: toolReducer,
+    history: historyReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(indexedDbSyncMiddleware),
