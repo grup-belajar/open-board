7c54fc088de5a89c343a128295f46097068a439c
novalCandra <dinatanovel5@gmail.com>
2026-09-10 21:20:18 +0700

    feat : ✅ Reapir Framgeent (Board)

--- src/app/board/[id]/page.tsx
+++ src/app/board/[id]/page.tsx
@@ -5,18 +5,24 @@ import WhiteboardCanvas from '../../../components/canvas/WhiteboardCanvas';
 import ButtonZoom from '../../../components/ui/button/ButtonZoom';
 import Sidebar from '../../../components/ui/Sidebar';
 import useKeyboardShortcuts from '../../../hooks/useKeyboardShortcuts';
+import FooterBoardPage from '@/src/components/ui/footers/FooterBoard';
 
 export default function BoardPage() {
   useKeyboardShortcuts();
 
   return (
-    <div className="flex h-screen flex-col overflow-hidden bg-gray-50">
-      <Navbar />
-      <div className="relative flex-1">
-        <WhiteboardCanvas />
-        <Sidebar />
+    <>
+
+      <div className="flex h-screen flex-col overflow-hidden bg-gray-50">
+        <Navbar />
+        <div className="relative flex-1">
+          <WhiteboardCanvas />
+          <Sidebar />
+        </div>
+        <ButtonZoom />
+        <ButtonShorcut />
       </div>
-      <ButtonZoom />
-    </div>
+      <FooterBoardPage />
+    </>
   );
 }
